47425f8e9da97736e05daf2e1c958438be3eb472
Freelancer Shahjalal <freelancershajalal@gmail.com>
2025-10-26 07:09:05 +0600

    Initial commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,35 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->id();
+    
+        Schema::create('users', function (Blueprint $table) {
+           $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
             $table->text('bio')->nullable();
             $table->string('profile_image')->nullable();
-            $table->string('provider_id')->nullable();
-            $table->string('provider_name')->nullable();
+
+            $table->string('provider_id')->nullable()->after('id');
+            $table->string('provider_name')->nullable()->after('provider_id');
+
             $table->rememberToken();
 
             $table->timestamp('created_at')->useCurrent();   
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
-        // Schema::create('users', function (Blueprint $table) {
-        //    $table->id();
-        //     $table->string('name');
-        //     $table->string('email')->unique();
-        //     $table->timestamp('email_verified_at')->nullable();
-        //     $table->string('password');
-        //     $table->text('bio')->nullable();
-        //     $table->string('profile_image')->nullable();
-        //     $table->string('provider_id')->nullable()->after('id');
-        //     $table->string('provider_name')->nullable()->after('provider_id');
-        //     $table->rememberToken();
-        //     $table->timestamp('created_at')->useCurrent();   
-        //     $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
-        // });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
